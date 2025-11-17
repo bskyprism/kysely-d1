09dae665796f3cd729e362e9f789a7af68bb07d8
nichoth <nichoth@nichoth.com>
2025-11-17 12:28:29 -0800

    Setup package, update dependencies

--- example/src/index.ts
+++ example/src/index.ts
@@ -8,8 +8,8 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { Kysely } from 'kysely';
-import { D1Dialect } from 'kysely-d1';
+import { Kysely } from 'kysely'
+import { D1Dialect } from 'kysely-d1'
 
 export interface Env {
   DB: D1Database;
@@ -25,54 +25,54 @@ interface Database {
 }
 
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    const { searchParams } = new URL(request.url);
-    const action = searchParams.get('action');
-    const key = searchParams.get('key');
-    const value = searchParams.get('value');
-    const db = new Kysely<Database>({
-      dialect: new D1Dialect({ database: env.DB }),
-    });
+    async fetch (request: Request, env: Env): Promise<Response> {
+        const { searchParams } = new URL(request.url)
+        const action = searchParams.get('action')
+        const key = searchParams.get('key')
+        const value = searchParams.get('value')
+        const db = new Kysely<Database>({
+            dialect: new D1Dialect({ database: env.DB }),
+        })
 
-    switch (action) {
-      case 'get': {
-        if (!key) {
-          return new Response('Key is not defined.', { status: 400 });
-        }
-        const result = await db.selectFrom('kv').selectAll().where('key', '=', key).executeTakeFirst();
-        if (!result) {
-          return new Response('', { status: 404 });
-        }
-        return new Response(result.value);
-      }
+        switch (action) {
+            case 'get': {
+                if (!key) {
+                    return new Response('Key is not defined.', { status: 400 })
+                }
+                const result = await db.selectFrom('kv').selectAll().where('key', '=', key).executeTakeFirst()
+                if (!result) {
+                    return new Response('', { status: 404 })
+                }
+                return new Response(result.value)
+            }
 
-      case 'set': {
-        if (!(key && value)) {
-          return new Response('Key and value must be defined.', { status: 400 });
-        }
-        try {
-          await db
-            .insertInto('kv')
-            .values([{ key, value }])
-            .onConflict((oc) => oc.column('key').doUpdateSet({ value }))
-            .execute();
-        } catch (err) {
-          console.log(err);
-          console.log((err as any).cause);
-          throw err;
-        }
-        return new Response(value, { status: 200 });
-      }
+            case 'set': {
+                if (!(key && value)) {
+                    return new Response('Key and value must be defined.', { status: 400 })
+                }
+                try {
+                    await db
+                        .insertInto('kv')
+                        .values([{ key, value }])
+                        .onConflict((oc) => oc.column('key').doUpdateSet({ value }))
+                        .execute()
+                } catch (err) {
+                    console.log(err)
+                    console.log((err as any).cause)
+                    throw err
+                }
+                return new Response(value, { status: 200 })
+            }
 
-      case 'delete': {
-        if (!key) {
-          return new Response('Key is not defined.', { status: 400 });
+            case 'delete': {
+                if (!key) {
+                    return new Response('Key is not defined.', { status: 400 })
+                }
+                await db.deleteFrom('kv').where('key', '=', key).execute()
+                return new Response('', { status: 200 })
+            }
         }
-        await db.deleteFrom('kv').where('key', '=', key).execute();
-        return new Response('', { status: 200 });
-      }
-    }
 
-    return new Response(`Action must be get/set/delete`, { status: 400 });
-  },
-};
+        return new Response('Action must be get/set/delete', { status: 400 })
+    },
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
+import type {
+    CompiledQuery,
+    DatabaseConnection,
+    DatabaseIntrospector,
+    Dialect,
+    Driver,
+    Kysely,
+    QueryCompiler,
+    QueryResult
+} from 'kysely'
 import {
-  CompiledQuery,
-  DatabaseConnection,
-  DatabaseIntrospector,
-  Dialect,
-  Driver,
-  Kysely,
-  SqliteAdapter,
-  SqliteIntrospector,
-  SqliteQueryCompiler,
-  QueryCompiler,
-  QueryResult,
-} from 'kysely';
-import type { D1Database } from '@cloudflare/workers-types';
+    SqliteAdapter,
+    SqliteIntrospector,
+    SqliteQueryCompiler
+} from 'kysely'
+import type { D1Database } from '@cloudflare/workers-types'
 
 /**
- * Config for the D1 dialect. Pass your D1 instance to this object that you bound in `wrangler.toml`.
+ * Config for the D1 dialect. Pass your D1 instance to this object that you
+ * bound in `wrangler.toml`.
  */
 export interface D1DialectConfig {
   database: D1Database;
@@ -22,9 +25,10 @@ export interface D1DialectConfig {
 
 /**
  * D1 dialect that adds support for [Cloudflare D1][0] in [Kysely][1].
- * The constructor takes the instance of your D1 database that you bound in `wrangler.toml`.
+ * The constructor takes the instance of your D1 database that you bound
+ * in `wrangler.toml`.
  *
- * ```typescript
+ * ```ts
  * new D1Dialect({
  *   database: env.DB,
  * })
@@ -34,114 +38,110 @@ export interface D1DialectConfig {
  * [1]: https://github.com/koskimas/kysely
  */
 export class D1Dialect implements Dialect {
-  #config: D1DialectConfig;
+    #config: D1DialectConfig
 
-  constructor(config: D1DialectConfig) {
-    this.#config = config;
-  }
+    constructor (config: D1DialectConfig) {
+        this.#config = config
+    }
 
-  createAdapter() {
-    return new SqliteAdapter();
-  }
+    createAdapter () {
+        return new SqliteAdapter()
+    }
 
-  createDriver(): Driver {
-    return new D1Driver(this.#config);
-  }
+    createDriver (): Driver {
+        return new D1Driver(this.#config)
+    }
 
-  createQueryCompiler(): QueryCompiler {
-    return new SqliteQueryCompiler();
-  }
+    createQueryCompiler (): QueryCompiler {
+        return new SqliteQueryCompiler()
+    }
 
-  createIntrospector(db: Kysely<any>): DatabaseIntrospector {
-    return new SqliteIntrospector(db);
-  }
+    createIntrospector (db: Kysely<any>): DatabaseIntrospector {
+        return new SqliteIntrospector(db)
+    }
 }
 
 class D1Driver implements Driver {
-  #config: D1DialectConfig;
+    #config: D1DialectConfig
 
-  constructor(config: D1DialectConfig) {
-    this.#config = config;
-  }
+    constructor (config: D1DialectConfig) {
+        this.#config = config
+    }
 
-  async init(): Promise<void> {}
+    async init (): Promise<void> {}
 
-  async acquireConnection(): Promise<DatabaseConnection> {
-    return new D1Connection(this.#config);
-  }
+    async acquireConnection (): Promise<DatabaseConnection> {
+        return new D1Connection(this.#config)
+    }
 
-  async beginTransaction(conn: D1Connection): Promise<void> {
-    return await conn.beginTransaction();
-  }
+    async beginTransaction (conn: D1Connection): Promise<void> {
+        return await conn.beginTransaction()
+    }
 
-  async commitTransaction(conn: D1Connection): Promise<void> {
-    return await conn.commitTransaction();
-  }
+    async commitTransaction (conn: D1Connection): Promise<void> {
+        return await conn.commitTransaction()
+    }
 
-  async rollbackTransaction(conn: D1Connection): Promise<void> {
-    return await conn.rollbackTransaction();
-  }
+    async rollbackTransaction (conn: D1Connection): Promise<void> {
+        return await conn.rollbackTransaction()
+    }
 
-  async releaseConnection(_conn: D1Connection): Promise<void> {}
+    async releaseConnection (_conn: D1Connection): Promise<void> {}
 
-  async destroy(): Promise<void> {}
+    async destroy (): Promise<void> {}
 }
 
 class D1Connection implements DatabaseConnection {
-  #config: D1DialectConfig;
-  //   #transactionClient?: D1Connection
+    #config: D1DialectConfig
 
-  constructor(config: D1DialectConfig) {
-    this.#config = config;
-  }
-
-  async executeQuery<O>(compiledQuery: CompiledQuery): Promise<QueryResult<O>> {
-    // Transactions are not supported yet.
-    // if (this.#transactionClient) return this.#transactionClient.executeQuery(compiledQuery)
-
-    const results = await this.#config.database
-      .prepare(compiledQuery.sql)
-      .bind(...compiledQuery.parameters)
-      .all();
-    if (results.error) {
-      throw new Error(results.error);
+    constructor (config: D1DialectConfig) {
+        this.#config = config
     }
 
-    const numAffectedRows = results.meta.changes > 0 ? BigInt(results.meta.changes) : undefined;
+    async executeQuery<O> (compiledQuery: CompiledQuery): Promise<QueryResult<O>> {
+        // Transactions are not supported yet.
 
-    return {
-      insertId:
-        results.meta.last_row_id === undefined || results.meta.last_row_id === null
-          ? undefined
-          : BigInt(results.meta.last_row_id),
-      rows: (results?.results as O[]) || [],
-      numAffectedRows,
-      // @ts-ignore deprecated in kysely >= 0.23, keep for backward compatibility.
-      numUpdatedOrDeletedRows: numAffectedRows,
-    };
-  }
+        const results = await this.#config.database
+            .prepare(compiledQuery.sql)
+            .bind(...compiledQuery.parameters)
+            .all()
 
-  async beginTransaction() {
-    // this.#transactionClient = this.#transactionClient ?? new PlanetScaleConnection(this.#config)
-    // this.#transactionClient.#conn.execute('BEGIN')
-    throw new Error('Transactions are not supported yet.');
-  }
+        if (results.error) {
+            throw new Error(results.error)
+        }
 
-  async commitTransaction() {
-    // if (!this.#transactionClient) throw new Error('No transaction to commit')
-    // this.#transactionClient.#conn.execute('COMMIT')
-    // this.#transactionClient = undefined
-    throw new Error('Transactions are not supported yet.');
-  }
+        const numAffectedRows = results.meta.changes > 0 ?
+            BigInt(results.meta.changes) :
+            undefined
 
-  async rollbackTransaction() {
-    // if (!this.#transactionClient) throw new Error('No transaction to rollback')
-    // this.#transactionClient.#conn.execute('ROLLBACK')
-    // this.#transactionClient = undefined
-    throw new Error('Transactions are not supported yet.');
-  }
+        const insertId = ((
+            results.meta.last_row_id === undefined ||
+            results.meta.last_row_id === null
+        ) ? undefined : BigInt(results.meta.last_row_id))
 
-  async *streamQuery<O>(_compiledQuery: CompiledQuery, _chunkSize: number): AsyncIterableIterator<QueryResult<O>> {
-    throw new Error('D1 Driver does not support streaming');
-  }
+        return {
+            insertId,
+            rows: (results?.results as O[]) || [],
+            numAffectedRows
+        }
+    }
+
+    async beginTransaction () {
+        throw new Error('Transactions are not supported yet.')
+    }
+
+    async commitTransaction () {
+        throw new Error('Transactions are not supported yet.')
+    }
+
+    async rollbackTransaction () {
+        throw new Error('Transactions are not supported yet.')
+    }
+
+    async * streamQuery<O> (
+        _compiledQuery:CompiledQuery,
+        _chunkSize:number
+    ):AsyncIterableIterator<QueryResult<O>> {
+        throw new Error('D1 Driver does not support streaming')
+    }
 }
